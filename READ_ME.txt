927fbace04b1ac66819ff8c8f66cd3ce3542a625
Carla <carlastaicu3@gmail.com>
2020-01-03 19:56:12 +0200

    working, starting point

--- READ_ME.txt
+++ READ_ME.txt
@@ -1,4 +1,1 @@
-to connect in terminal with the database
-    mongo
-    use dbname
-    db.todo.insert({todo : "add whatever"})
+START => npm start
---
+++ models/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb://localhost:27017/EmployeeDB";
+
+mongoose.connect(url,
+    {useNewUrlParser:true,
+    useUnifiedTopology: true},
+        (err) => {
+        if(!err){ console.log("MongoDB Connection Succeeded");}
+        else{
+            console.log("An Error Occured");
+        } 
+})
+
+require('./employee.model');
